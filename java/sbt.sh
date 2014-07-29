b56f88effbc6ba5a3fe67282349b46639bf1c3da
hperadin <hperadin@element.hr>
2014-07-29 12:08:53 +0200

    Recursive loading of branch names

--- java/sbt.sh
+++ java/sbt.sh
@@ -1,2 +1,14 @@
-#!/bin/sh
-java -DPropsLoader.branch=master -Xss2m -Xms2g -Xmx2g -jar project/strap/gruj_vs_sbt-launch-0.13.x.jar "$*"
+#!/bin/bash
+
+path=$(pwd)
+
+while [ "$(readlink -f $path)" != "/" ]; do
+    for branch_file in $(find $path -maxdepth 1 -mindepth 1 -name "*.branch"); do
+	content=$(cat "$branch_file" | tr -d '\r\n')
+	name=$(basename "$branch_file" | tr '.[a-z]' '_[A-Z]')
+	branch_data="$branch_data -D$name=\"$content\""
+    done
+    path="$path/../"
+done
+
+java $branch_data -Xss2m -Xms2g -Xmx2g -jar project/strap/gruj_vs_sbt-launch-0.13.x.jar \"$*\"
